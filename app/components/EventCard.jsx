e2ba3afc4c6bcc125161d31ed25f5485faafab7d
George Constantin <george.constantin33@gmail.com>
2025-04-24 09:18:14 +0100

    image fix for mobile

--- app/components/EventCard.jsx
+++ app/components/EventCard.jsx
@@ -27,7 +27,7 @@ export default function EventCard({
             style={styles.tinyLogo}
             source={
               image_url
-                ? image_url
+                ? { uri: image_url }
                 : {
                     uri: 'https://media.istockphoto.com/id/1396814518/vector/image-coming-soon-no-photo-no-thumbnail-image-available-vector-illustration.jpg?s=612x612&w=0&k=20&c=hnh2OZgQGhf0b46-J2z7aHbIWwq8HNlSDaNp2wn_iko=',
                   }
